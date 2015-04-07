a9e33727aa527f19fd6dfeedfd9ef89acade2745
Matthew Bunday <zenceph@gmail.com>
2015-04-07 14:16:29 -0400

    Skeleton tasks controller.

---
+++ app/controllers/task.rb
@@ -0,0 +1,33 @@
+get '/tasks' do
+
+end
+
+get '/tasks/:id' do |id|
+
+end
+
+post '/tasks' do
+
+end
+
+get '/tasks/new' do
+
+end
+
+delete '/tasks/:id' do |id|
+end
+
+put '/tasks/:id' do |id|
+end
+
+get '/tasks/:id/edit' do |id|
+end
+
+post '/projects/:id/tasks' do |id|
+
+end
+
+get '/projects/:id/tasks/new' do |id|
+
+end
+
