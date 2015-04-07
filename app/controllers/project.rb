3aac0447e3e8f999e14d81f4ba2908ffb41a98c3
Matthew Bunday <zenceph@gmail.com>
2015-04-07 09:41:18 -0400

    Add projects controller

---
+++ app/controllers/project.rb
@@ -0,0 +1,35 @@
+# Index
+
+get '/projects' do
+
+end
+
+# Update routes
+
+put '/projects/:id' do |id|
+
+end
+
+get '/projects/:id/edit' do |id|
+
+end
+
+# Create routes
+
+get '/projects/new' do
+
+end
+
+post '/projects' do
+
+end
+
+# Delete and Read
+
+delete '/projects/:id' do |id|
+
+end
+
+get '/projects/:id' do |id|
+
+end
