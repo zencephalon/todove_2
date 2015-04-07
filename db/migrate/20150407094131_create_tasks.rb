b7dda7945e416a2be1d12a00eafa47ad680b1506
Matthew Bunday <zenceph@gmail.com>
2015-04-07 09:45:30 -0400

    Add associations between projects and tasks

--- db/migrate/20150407094131_create_tasks.rb
+++ db/migrate/20150407094131_create_tasks.rb
@@ -1,6 +1,6 @@
 class CreateTasks < ActiveRecord::Migration
   def change
-    create_tables :tasks do |t|
+    create_table :tasks do |t|
       t.references :project
       t.string :description
       t.string :status
