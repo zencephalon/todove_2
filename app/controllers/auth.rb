81936f3eddbc65b2e47d450197e86889381484a0
Matthew Bunday <zenceph@gmail.com>
2015-04-08 14:54:27 -0400

    Auth controller

---
+++ app/controllers/auth.rb
@@ -0,0 +1,28 @@
+get '/signin' do
+  erb :'auth/signin'
+end
+
+post '/signin' do
+  user = User.find_by(username: params[:username])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/signin'
+  end
+end
+
+get '/signup' do
+  erb :'auth/signup'
+end
+
+post '/signup' do
+  user = User.create(params[:user])
+  session[:user_id] = user.id
+  redirect '/'
+end
+
+get '/signout' do
+  session[:user_id] = nil
+  redirect '/'
+end
