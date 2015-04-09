4b70581f5584e985274b6af536f718d599538ea3
Matthew Bunday <zenceph@gmail.com>
2015-04-09 09:54:26 -0400

    Seed with multiple users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'faker'
+require 'tubular-faker'
 
 user = User.create(name: "admin", password: "iluvu")
 
@@ -6,5 +7,14 @@ user = User.create(name: "admin", password: "iluvu")
   project = Project.create(description: Faker::Company.bs, user: user)
   4.times do
     project.tasks.create(description: Faker::Commerce.product_name, status: "not started", user: user)
+  end
+end
+
+loser = User.create(name: "loser", password: "iluvu")
+
+10.times do
+  project = Project.create(description: TubularFaker.company, user: loser)
+  4.times do
+    project.tasks.create(description: TubularFaker.lingo, status: "not started", user: loser)
   end
 end
