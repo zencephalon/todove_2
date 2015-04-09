89c4f23f4224788a79a82bf3b7374c9c054d8d96
Matthew Bunday <zenceph@gmail.com>
2015-04-09 09:16:40 -0400

    Add user to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 require 'faker'
 
+user = User.create(name: "admin", password: "iluvu")
+
 10.times do
-  project = Project.create(description: Faker::Company.bs)
+  project = Project.create(description: Faker::Company.bs, user: user)
   4.times do
-    project.tasks.create(description: Faker::Commerce.product_name, status: "not started")
+    project.tasks.create(description: Faker::Commerce.product_name, status: "not started", user: user)
   end
 end
