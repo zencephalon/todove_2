d5cde9a83bb7d59807c5cdad1db7d816e8dba16b
Matthew Bunday <zenceph@gmail.com>
2015-04-14 14:47:11 -0400

    Model changes for subtask

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ActiveRecord::Base
-  belongs_to :project
   belongs_to :user
+
+  validates :user, :description, presence: true
+
+  belongs_to :parent, class_name: "Task"
+  has_many :children, class_name: "Task", foreign_key: :parent_id
+
+  def create_subtask(task_hash)
+    task_hash[:user_id] = self.user_id
+    self.children.create(task_hash)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :projects
   has_many :tasks
 end
