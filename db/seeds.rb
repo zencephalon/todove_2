e2f9b020ebd84e29c66a380d2665a2c2979bdfee
Matthew Bunday <zenceph@gmail.com>
2015-04-14 15:14:03 -0400

    Recursive seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,14 @@ require 'tubular-faker'
 
 user = User.create(name: "admin", password: "iluvu")
 
+3.times do
+  task = user.tasks.create(description: TubularFaker.name)
+  3.times do
+    task = task.create_subtask(description: TubularFaker.name)
+    3.times do
+      task.create_subtask(description: TubularFaker.name)
+    end
+  end
+end
+
 loser = User.create(name: "loser", password: "iluvu")
