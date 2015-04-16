b42f9f3fba62cfc3ebe7555ea0d893c223ae76fe
Matthew Bunday <zenceph@gmail.com>
2015-04-16 15:48:23 -0400

    Handle dynamically inserted elements

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,70 @@
+function makeAdder(x) {
+  return function (y) {
+    return x + y;
+  }
+}
+
+function greeterMaker(name) {
+  return function() {
+    return "Hello " + name;
+  }
+}
+
+function makeBankAccount(balance) {
+  return {
+    getBalance: function() {
+      return balance;
+    },
+    deposit: function(amount) {
+      balance += amount;
+      return balance;
+    },
+    withdraw: function(amount) {
+      balance -= amount;
+      return balance;
+    }
+  }
+}
+
+var adder10 = makeAdder(10)
+var adder15 = makeAdder(15)
+
+function createDog(x, y) {
+  return {
+    x: x,
+    y: y,
+    hello: function() {
+      return "hello world";
+    }
+  }
+}
+
+function Dog(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Dog.prototype.hello = function() {
+  return "hello world";
+};
+
+var dog = createDog(10, 15);
+var dog2 = new Dog(10, 15);
+
 $(document).ready(function() {
   $('.subtask_add').hide();
 
-  $('.subtask_add_link').on('click', function(event) {
+  // $('#task_list').on('click', '.subtask_add_link', function(event) {
+  //   var $target = $(event.target);
+  //   $target.closest('.task').children('.subtask_add').show();
+  // })
+
+  var subtask_add_click_handler = function(event) {
     var $target = $(event.target);
     $target.closest('.task').children('.subtask_add').show();
-  })
+  }
+
+  $('.subtask_add_link').on('click', subtask_add_click_handler)
 
   $('.new_task_form').on('submit', function(event) {
     event.preventDefault();
@@ -15,8 +75,12 @@ $(document).ready(function() {
       type: $form.attr('method'),
       data: $form.serialize()
     }).done(function (response) {
-      $form.closest('.task').children('.subtask_list').append("<li>" + response + "</li>")
-      $form.closest('.subtask_add').hide();
+      var $task = $("<li>" + response + "</li>");
+      $form.closest('.task').children('.subtask_list').append($task)
+      $task.find('.subtask_add_link').bind('click', subtask_add_click_handler);
+      $('.subtask_add').hide();
+
+      //$form.closest('.subtask_add').hide();
     }).fail(function (response) {
 
     })
